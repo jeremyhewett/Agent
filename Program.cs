e78f26c73185b435064b14167037a8ca1ed21129
Jeremy Hewett <jeremyhewett@gmail.com>
2018-01-31 15:50:57 -0800

    Added basic SocketIoClientDotNet client.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Agent
+﻿using System;
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace Agent
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var socket = IO.Socket("http://localhost:8000");
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+                Console.WriteLine("Connected");
+            });
+
+            socket.On("command", (data) =>
+            {
+                Console.WriteLine(data);
+                socket.Emit("response");
+                socket.Disconnect();
+            });
+            Console.ReadLine();
         }
     }
 }
